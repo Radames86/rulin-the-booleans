d6c89de5bd3e50fd3c3fe1628bdfc5bb973201ba
Radames Rivera <rrivera@perseverenow.org>
2025-05-21 12:08:04 -0500

    first

--- main.js
+++ main.js
@@ -1,9 +1,48 @@
 /********************
  * YOUR CODE BELOW! *
  ********************/
-
-
-
+function moreThan5(num){
+  return num > 5
+}
+function isNewTopScore(score, topScore){
+ return score > topScore
+}
+function isInDanger(grade){
+return grade >= 60 && grade <= 71;
+}
+function isCoasting(grade){
+return grade >= 72 && grade <= 83;
+}
+function isSucceeding(grade){
+return grade >= 84 && grade <= 92;
+}
+function isFailing(grade){
+return grade < 60;
+}
+function isAcing(grade){
+return grade > 92;
+}
+function isStudent(role){
+return role === "student";
+}
+function isTeacher(role){
+return role === "teacher";
+}
+function isAdmin(role){
+return role === "admin";
+}
+function isElementary(schoolLevel){
+return schoolLevel === "elementary";
+}
+function areDifferentPeople(name1, name2){
+return name1 !== name2; 
+}
+function isMiddleSchoolTeacher(role, level){
+return role === "teacher" && level >= 6 && level <= 8;
+}
+function notAnElementarySchoolAdministrator(schoolLevel, role){
+return schoolLevel !== "elementary" || role !==  "admin";
+}
 /********************************************************************************************
  * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
  *                              THIS IS FOR INTERNAL USE ONLY                               *
